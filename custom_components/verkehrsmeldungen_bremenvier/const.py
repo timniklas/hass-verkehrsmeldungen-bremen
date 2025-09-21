0d7b1bd40ea44841790819290cb77e7c5094dc8e
timniklas <accounts@tnkoch.de>
2025-09-21 17:20:22 +0200

    Update const.py

--- custom_components/verkehrsmeldungen_bremenvier/const.py
+++ custom_components/verkehrsmeldungen_bremenvier/const.py
@@ -1,5 +1,5 @@
 DOMAIN = "verkehrsmeldungen_bremenvier"
-TRAFFIC_URL = "https://www.bremenvier.de/verkehr/aktuelle-verkehrsmeldungen-aus-dem-land-bremen-und-der-region-100.html"
+TRAFFIC_URL = "https://www.bremenvier.de/verkehr/index.html"
 DE_MONTHS = {
     "januar": 1, "februar": 2, "märz": 3, "maerz": 3, "april": 4, "mai": 5, "juni": 6,
     "juli": 7, "august": 8, "september": 9, "oktober": 10, "november": 11, "dezember": 12
